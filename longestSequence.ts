c9fa8dbf8ed24f370847cf3c706f89d60bd3a0b9
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-03-25 10:51:51 +0000

    add function to find longest consecutive sequence

---
+++ longestSequence.ts
@@ -0,0 +1,22 @@
+function longestConsecutive(nums: number[]): number {
+    if (!nums.length) return 0
+    nums.sort((a, b) => a - b)
+    let longestSequence = 1
+    let currentSequence = 1
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === nums[i - 1]) { continue }
+        else if (nums[i] - 1 === nums[i - 1]) {
+            currentSequence++
+            if (currentSequence > longestSequence) {
+                longestSequence = currentSequence
+            }
+        }
+        else {
+            if (currentSequence > longestSequence) {
+                longestSequence = currentSequence
+            }
+            currentSequence = 1
+        }
+    }
+    return longestSequence
+};

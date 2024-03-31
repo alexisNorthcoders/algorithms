cccef2c09e504c07f58ccec4e14e4dd39108b0af
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-03-31 02:05:20 +0100

    add tests for topKFrequent algorithm

--- topKFrequent/topKFrequent.test.ts
+++ topKFrequent/topKFrequent.test.ts
@@ -1,9 +1,15 @@
 import topKFrequent from "./topKFrequent";
 
 describe('topKFrequent', () => {
-    it('', () => {
-        
-        expect().toBe();
+    it('return most frequent 2 integers', () => {
+        const input = [1,1,1,2,2,3]
+        const k = 2
+        expect(topKFrequent(input,k)).toEqual([1,2]);
+    });
+    it('return most frequent int', () => {
+        const input = [3,0,1,0]
+        const k = 1
+        expect(topKFrequent(input,k)).toEqual([0]);
     });
 
 });
--- topKFrequent/topKFrequent.ts
+++ topKFrequent/topKFrequent.ts
@@ -1,1 +1,19 @@
-export default function topKFrequent (){}
+export default function topKFrequent(input: number[], k: number) {
+    const map = new Map()
+    for (let i = 0; i < input.length; i++) {
+        let value = map.get(input[i])
+        
+        if (value) {
+            value++
+            map.set(input[i], value)
+            
+        }
+        else {
+            map.set(input[i], 1)
+        }
+    }
+    const sortedEntries = Array.from(map.entries()).sort((a, b) => b[1] - a[1]);
+    const result = sortedEntries.map(entry=>entry[0]).slice(0,k)
+    return result
+
+}

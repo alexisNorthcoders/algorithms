804dbb55c649e8502381cf967240e93a3eb50a7e
alexisNorthcoders <alexiscreoulo@hotmail.com>
2024-04-02 12:59:37 +0100

    add binary search solution to minEatingSpeed and test cases

--- minEatingSpeed/minEatingSpeed.test.ts
+++ minEatingSpeed/minEatingSpeed.test.ts
@@ -1,4 +1,4 @@
-import minEatingSpeed from "./minEatingSpeed";
+import {minEatingSpeed,binMinEatingSpeed} from "./minEatingSpeed"
 
 describe('minEatingSpeed', () => {
     it('', () => {
@@ -19,11 +19,53 @@ describe('minEatingSpeed', () => {
         
         expect(minEatingSpeed(piles,h)).toBe(23);
     });
-    it('efficiency check', () => {
+    it('efficiency check bruteforce', () => {
         const piles = [332484035,524908576,855865114,632922376,222257295,690155293,112677673,679580077,337406589,290818316,877337160,901728858,679284947,688210097,692137887,718203285,629455728,941802184]
         const h = 823855818
         
         expect(minEatingSpeed(piles,h)).toBe(14);
     });
+    it('efficiency check bruteforce', () => {
+        const piles = [1000000000] 
+        const h = 2
+        
+        expect(minEatingSpeed(piles,h)).toBe(500000000);
+    });
+   
+
+});
+describe('binMinEatingSpeed', () => {
+    it('', () => {
+        const piles = [3,6,7,11]
+        const h = 8
+        
+        expect(binMinEatingSpeed(piles,h)).toBe(4);
+    });
+    it('', () => {
+        const piles = [30,11,23,4,20]
+        const h = 5
+        
+        expect(binMinEatingSpeed(piles,h)).toBe(30);
+    });
+    it('', () => {
+        const piles = [30,11,23,4,20]
+        const h = 6
+        
+        expect(binMinEatingSpeed(piles,h)).toBe(23);
+    });
+  
+    it('efficiency check binary', () => {
+        const piles = [332484035,524908576,855865114,632922376,222257295,690155293,112677673,679580077,337406589,290818316,877337160,901728858,679284947,688210097,692137887,718203285,629455728,941802184]
+        const h = 823855818
+        
+        expect(binMinEatingSpeed(piles,h)).toBe(14);
+    });
+    it('efficiency check binary', () => {
+        const piles = [1000000000] 
+        const h = 2
+        
+        expect(binMinEatingSpeed(piles,h)).toBe(500000000);
+    });
+   
 
 });
--- minEatingSpeed/minEatingSpeed.ts
+++ minEatingSpeed/minEatingSpeed.ts
@@ -1,18 +1,14 @@
 // brute force solution
-export default function minEatingSpeed(piles: number[], h: number): number {
+export function minEatingSpeed(piles: number[], h: number): number {
     let k = 1
     let lapsedTime = 0
     while (true) {
         for (let i = 0; i < piles.length; i++) {
             let currentBananaPile = piles[i]
-            while (currentBananaPile > 0) {
-                currentBananaPile -= k
-                lapsedTime++
-                if (i === piles.length - 1 && currentBananaPile <= 0) {
-                    console.log(k)
-                    return k
-                }
-                else if (lapsedTime >= h) { break }
+            lapsedTime += Math.ceil(currentBananaPile / k)
+            if (i === piles.length - 1 && lapsedTime <= h) {
+                console.log(k)
+                return k
             }
             if (lapsedTime >= h) {
                 k++
@@ -21,4 +17,36 @@ export default function minEatingSpeed(piles: number[], h: number): number {
             }
         }
     }
+}
+
+// binary search solution
+
+export function binMinEatingSpeed(piles: number[], h: number): number {
+    piles.sort((a, b) => a - b)
+    let lapsedTime = 0
+    let startK = 1
+    let endK = piles[piles.length - 1]
+    let result = endK
+
+    while (startK <= endK) {
+        const k = Math.floor((startK + endK) / 2)
+
+        for (let i = 0; i < piles.length; i++) {
+            let currentBananaPile = piles[i]
+            lapsedTime += Math.ceil(currentBananaPile / k)
+        }
+        if (lapsedTime <= h) {
+            endK = k - 1
+            lapsedTime = 0
+            if (k < result) {
+                result = k
+            }
+
+        }
+        else if (lapsedTime > h) {
+            startK = k + 1
+            lapsedTime = 0
+        }
+    }
+    return result
 }
